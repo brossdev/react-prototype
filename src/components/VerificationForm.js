12e464c2cec54faca7dd0827e6dbe4d5a4e3f756
Bobby Ross <bobby.dev@pm.me>
2020-09-02 21:49:41 +0100

    style: add basic styling to verification form

--- src/components/VerificationForm.js
+++ src/components/VerificationForm.js
@@ -10,13 +10,20 @@ const VerificationForm = ({ onSubmit }) => {
   };
 
   return (
-    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+    <form
+      className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      onSubmit={handleSubmit}
+    >
       <h2>Confirm Verification Code</h2>
       <div>
         <div className="mb-4">
           <label htmlFor="verificationCode">
             <span>Code</span>
-            <input id="verificationCode" type="number" />
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="verificationCode"
+              type="number"
+            />
           </label>
         </div>
         <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
